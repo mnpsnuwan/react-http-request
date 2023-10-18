2a81ce6c818f13eedc49852d4850a6925253afaf
mnpsnuwan <nuwanp124@gmail.com>
2023-10-18 13:34:55 -0700

    IM:DEV:React Http Request - Http Request to Get Data.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,8 +4,18 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from "axios";
 
 class Blog extends Component {
+
+    componentDidMount() {
+        const posts = axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response =>  {
+                console.log(response);
+            })
+
+    }
+
     render () {
         return (
             <div>
