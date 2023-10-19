c3872650c81e900c68bb197d091ab92c78f7b6cb
mnpsnuwan <nuwanp124@gmail.com>
2023-10-19 11:37:59 -0700

    IM:DEV:React Http Request - Delete Posts and Handling Errors.

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -20,6 +20,13 @@ class FullPost extends Component {
 
     }
 
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                console.log(response);
+            });
+    }
+
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
         if(this.props.id){
@@ -31,7 +38,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
                     </div>
                 </div>
             );
