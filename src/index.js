8de052fbf4923fed08de93bf1174453f32e0aec4
mnpsnuwan <nuwanp124@gmail.com>
2023-10-19 12:14:39 -0700

    IM:DEV:React Http Request - Setting a Default Global Configurations for Axios.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from "axios";
 
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 axios.interceptors.request.use(request => {
     console.log(request);
     // Edit request config
