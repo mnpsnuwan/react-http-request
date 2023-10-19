19fe929a16c9aee5a2a01fb532b815a05152b187
mnpsnuwan <nuwanp124@gmail.com>
2023-10-19 09:16:43 -0700

    IM:DEV:React Http Request - Fetching Data on Update.

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 
 import './FullPost.css';
+import axios from "axios";
 
 class FullPost extends Component {
+    state = {
+        loadedPost: null,
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(this.props.id){
+            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)){
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                    .then(response => {
+                        this.setState({loadedPost: response.data});
+                    })
+            }
+        }
+
+    }
+
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
         if(this.props.id){
+            post = <p style={{textAlign: 'center'}}>Loading...</p>;
+        }
+        if(this.state.loadedPost){
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
