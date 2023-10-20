1adaa1d390097bf9b3baf8b4b1bfc94317cf14fd
mnpsnuwan <nuwanp124@gmail.com>
2023-10-20 16:22:09 -0700

    IM:DEV:React Http Request - Setting up Nav Links.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -53,7 +53,15 @@ class Blog extends Component {
         }
 
         return (
-            <div>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
                 </section>
