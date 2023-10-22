5f24fb2dfec1ba7cb9a9e4a58c3ad2b833d2c2d9
mnpsnuwan <nuwanp124@gmail.com>
2023-10-21 23:28:57 -0700

    IM:DEV:React Http Request - Switching Between Pages.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import './Blog.css';
 import Posts from "./Posts/Posts";
+import {Route, Routes} from "react-router-dom";
+import NewPost from "./NewPost/NewPost";
 
 class Blog extends Component {
     render () {
@@ -16,7 +18,12 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                {/*<Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" exact render={() => <h1>Home 2</h1>} />*/}
+                <Routes>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" component={NewPost} />
+                </Routes>
             </div>
         );
     }
