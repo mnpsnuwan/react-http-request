ff65084c0d51d84d793092bfd184d59a7edd58b7
mnpsnuwan <nuwanp124@gmail.com>
2023-11-10 16:18:37 -0700

    IM:DEV:React Http Request - Creating Dynamic Nested Routes.

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -10,19 +10,26 @@ class FullPost extends Component {
 
     componentDidMount() {
         console.log(this.props);
+        this.loadData()
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.loadData()
+    }
+
+    loadData(){
         if(this.props.match.params.id){
-            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)){
+            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.match.params.id)){
                 axios.get('/posts/' + this.props.match.params.id)
                     .then(response => {
                         this.setState({loadedPost: response.data});
                     })
             }
         }
-
     }
 
     deletePostHandler = () => {
-        axios.delete('/posts/' + this.props.id)
+        axios.delete('/posts/' + this.props.match.params.id)
             .then(response => {
                 console.log(response);
             });
@@ -30,7 +37,7 @@ class FullPost extends Component {
 
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
-        if(this.props.id){
+        if(this.props.match.params.id){
             post = <p style={{textAlign: 'center'}}>Loading...</p>;
         }
         if(this.state.loadedPost){
