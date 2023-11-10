dc8c14cfb229b08200efdad96030fbe25a9e3508
mnpsnuwan <nuwanp124@gmail.com>
2023-11-10 12:15:22 -0700

    IM:DEV:React Http Request - Navigating Programmatically.

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import axios from "../../../axios";
-import {Link} from "react-router-dom";
 
 import Post from "../../../components/Post/Post";
 import './Posts.css'
@@ -32,7 +31,8 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
+        // this.props.history.push({pathname: '/' + id})
+        this.props.history.push('/' + id)
     }
 
     render() {
@@ -40,12 +40,13 @@ class Posts extends Component {
         if(!this.state.error){
             posts = this.state.posts.map(post => {
                 return (
-                    <Link to={'/' + post.id} key={post.id}>
-                        <Post
-                            title={post.title}
-                            author={post.author}
-                            clicked={() => this.postSelectedHandler(post.id)}/>
-                    </Link>
+                    // <Link to={'/' + post.id} key={post.id}>
+                    <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)}/>
+                    // </Link>
                 );
             });
         }
