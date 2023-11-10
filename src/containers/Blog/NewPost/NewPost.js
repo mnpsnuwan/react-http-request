fa71bb2503339cbacdc625aa4dd5e9f9091dc37b
mnpsnuwan <nuwanp124@gmail.com>
2023-11-10 16:38:53 -0700

    IM:DEV:React Http Request - Conditional Redirect.

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 
 import './NewPost.css';
 import axios from "axios";
+import {Redirect} from "react-router-dom";
 
 class NewPost extends Component {
 
     state= {
         title: '',
         content: '',
-        author: 'Nuwan'
+        author: 'Nuwan',
+        submitted: false
     }
 
     componentDidMount() {
@@ -24,12 +26,18 @@ class NewPost extends Component {
         axios.post('/posts', data)
             .then(response => {
                 console.log(response)
+                this.setState({submitted: true});
             })
     }
 
     render () {
+        let redirect = null;
+        if (this.state.submitted){
+            redirect = <Redirect to="/posts"/>
+        }
         return (
             <div className="NewPost">
+                {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
