b292b2ede61ca1dc7da57f1afa63fe5342607493
mnpsnuwan <nuwanp124@gmail.com>
2023-11-11 18:21:46 -0700

    IM:DEV:React Http Request - Using History Prop to Redirect.

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -26,7 +26,9 @@ class NewPost extends Component {
         axios.post('/posts', data)
             .then(response => {
                 console.log(response)
-                this.setState({submitted: true});
+                this.props.history.replace("/posts");
+                // this.props.history.push("/posts");
+                // this.setState({submitted: true});
             })
     }
 
