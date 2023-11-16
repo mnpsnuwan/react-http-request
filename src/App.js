261762e63c049328fbe04e2643b1f56bc04bce98
mnpsnuwan <nuwanp124@gmail.com>
2023-11-15 17:04:07 -0700

    IM:DEV:React Http Request - Routing when deploy to server.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+        // <BrowserRouter basename="/my-app">
         <BrowserRouter>
           <div className="App">
             <Blog />
